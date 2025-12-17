4df03cbcc7a74a7ffe10fcb10c220821dcf10b35
dedmanwalking <dedmanwalking@proton.me>
2025-12-17 20:36:46 +0000

    Add files via upload
    added general naming scheme

--- NOAHZK_bigint_lib/noahzk_bigint.h
+++ NOAHZK_bigint_lib/noahzk_bigint.h
@@ -16,4 +16,13 @@
 #include "ops/mul.h"
 #include "ops/sub.h"
 
+// NAMING SCHEME:
+//      NOAHZK_variable_width_<op>
+//      for arithmetic ops, the pointers to the source arguments are named:
+// 1  arg  ~ src
+// 2+ args ~ rs0, ..., rsn
+//      multiple dsts are named as:
+// 1  dst  ~ dst
+// 2+ dsts ~ dst0, ..., dstn
+
 #endif
