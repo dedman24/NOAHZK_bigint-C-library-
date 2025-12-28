8bd1bde434ef66d952d57d1012038e47106bd3ac
dedmanwalking <dedmanwalking@proton.me>
2025-12-28 20:50:54 +0000

    Add files via upload
    updated code guidelines

--- NOAHZK_bigint_lib/noahzk_bigint.h
+++ NOAHZK_bigint_lib/noahzk_bigint.h
@@ -24,5 +24,14 @@
 //      multiple dsts are named as:
 // 1  dst  ~ dst
 // 2+ dsts ~ dst0, ..., dstn
+//      all operations ending in _resize are NOT GUARANTEED TO BE constant-time.
+//      all other operations do not guarantee correctness when the source operands are larger in width than the destination operand.
+// CODING STYLE:
+//      variables and pointers declared const when possible
+//      pointers declared restrict when possible
+//      const before restrict on pointers (not that it changes much, I just like it more this way)
+//  foo(void* restrict const bar);  --> NO
+//  foo(void* const restrict bar);  --> YES
+//      code should be C99-compatible (the best C standard)
 
 #endif
